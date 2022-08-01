109edf0db10d8f17f7fe7a17a7219263e17c531d
cliff.folsom <cliff.folsom@decodeadvertising.com>
2022-08-01 16:14:38 -0500

    adding homepage url

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,5 +1,8 @@
 // import React from 'react'
 import './Portfolio.css'
+import IMG1 from '../../assets/pw-gen_41.png'
+import IMG2 from '../../assets/tictactoe_40.png'
+import IMG3 from '../../assets/cat-typing1.jpg'
 
 const Portfolio = () => {
   return (
@@ -10,9 +13,45 @@ const Portfolio = () => {
        <div className='container portfolio__container'>
         <article className='portfolio__item'>
           <div className='portfolio__item-image'>
+            <img src={IMG1} alt="React Password Generator" />
+          </div>
           <h3>Password Generator</h3>
-         </div> 
+          <div className='portfolio__item-cta'>
+          <a href="https://github.com/cliff-folsom/pw-gen" target="_blank" rel='noreferrer' className='btn'>Github</a>
+          <a href="https://fervent-volhard-f2991a.netlify.app/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+          </div>
         </article>  
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt="React Tic-Tac-Toe" />
+          </div>
+          <h3>Tic-tac-toe</h3>
+          <div className='portfolio__item-cta'>
+          <a href="https://github.com/cliff-folsom/tic-tac-toe" target="_blank" rel='noreferrer' className='btn'>Github</a>
+          <a href="https://github.com/cliff-folsom/tic-tac-toe" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+          </div>
+        </article>  
+
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG3} alt="React Password Generator" />
+          </div>
+          <h3>Password Generator</h3>
+          <a href="https://github.com/cliff-folsom/pw-gen" target="_blank" rel='noreferrer' className='btn'>Github</a>
+          <a href="https://fervent-volhard-f2991a.netlify.app/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+          
+        </article>  
+{/*
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="React Password Generator" />
+          </div>
+          <h3>Password Generator</h3>
+          <a href="https://github.com/cliff-folsom/pw-gen" target="_blank" rel='noreferrer' className='btn'>Github</a>
+          <a href="https://fervent-volhard-f2991a.netlify.app/" target="_blank" rel='noreferrer' className='btn btn-primary'>Live Demo</a>
+          
+  </article> */}  
       </div>
     </section> 
   )
