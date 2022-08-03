56feeb03b990120d80f574c0e5e1f22ca44cbb58
cliff.folsom <cliff.folsom@decodeadvertising.com>
2022-08-03 14:32:24 -0500

    Commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,11 @@ import Portfolio from './components/Portfolio/Portfolio'
 import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
-import Favicon from "react-favicon";
-import img from './assets/cliff1.jpg'
 
 
 const App = () => {
   return (
     <>
-        <div>
-          <Favicon url={img} />
-        </div>
         <Header />
         <Nav />
         <About />
