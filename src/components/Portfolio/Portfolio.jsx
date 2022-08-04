2ce25ff5b7e6adafddf13e4f5572f5fecd1a935c
cliff.folsom <cliff.folsom@decodeadvertising.com>
2022-08-04 11:46:06 -0500

    fixed image sizing in portfolio section

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import './Portfolio.css'
 import IMG1 from '../../assets/pw-gen_41.png'
-import IMG2 from '../../assets/tictactoe_40.png'
-import IMG3 from '../../assets/recipe_50.jpg'
+import IMG2 from '../../assets/tictactoe_50.png'
+import IMG3 from '../../assets/recipe_70.jpg'
 
 const Portfolio = () => {
   return (
