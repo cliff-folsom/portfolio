c0d9c9ab8139a38cabfdb604c5319291a2d9a3c5
cliff.folsom <cliff.folsom@decodeadvertising.com>
2022-08-08 16:52:43 -0500

    build

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import About from './components/About/About'
 import Experience from './components/Experience/Experience'
 import Services from './components/Services/Services'
 import Portfolio from './components/Portfolio/Portfolio'
-import Testimonials from './components/Testimonials/Testimonials'
 import Contact from './components/Contact/Contact'
 import Footer from './components/Footer/Footer'
 
@@ -19,7 +18,6 @@ const App = () => {
         <Experience />
         <Services />
         <Portfolio />
-        <Testimonials />
         <Contact />
         <Footer />
     </>
